81d4eb40c6de4536adc8614fd6ba33acf4dd3df1
David Plappert <david@davidplappert.com>
2022-09-10 01:42:17 -0500

    fix

--- lib/active_record/connection_adapters/planetscale-api-ruby/mysql2.rb
+++ lib/active_record/connection_adapters/planetscale-api-ruby/mysql2.rb
@@ -1,7 +1,7 @@
-require 'active_record/connection_adapters/aurora_serverless/mysql2/result'
-require 'active_record/connection_adapters/aurora_serverless/mysql2/client'
-require 'active_record/connection_adapters/aurora_serverless/mysql2/connection_handling'
-# require 'active_record/connection_adapters/aurora_serverless/gem_hack'
+require 'active_record/connection_adapters/planetscale-api-ruby/mysql2/result'
+require 'active_record/connection_adapters/planetscale-api-ruby/mysql2/client'
+require 'active_record/connection_adapters/planetscale-api-ruby/mysql2/connection_handling'
+# require 'active_record/connection_adapters/planetscale-api-ruby/gem_hack'
 require 'active_record/connection_adapters/mysql2_adapter'
 
 module ActiveRecord
